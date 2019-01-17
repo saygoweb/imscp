fe1a9e3ab3824272a8d5ea1a9147b07f9989ddd9
Laurent Declercq <l.declercq@nuxwin.com>
2019-01-17 19:48:08 +0100

    Added: Support for PHP 7.2 , 7.3 (backend) Added: Packages for PHP 7.2, 7.3 (all distributions)
    (cherry picked from commit 0fdb85710edc82f065f7e716c311f443ea9c4ab8)

--- engine/PerlLib/Servers/httpd/apache_fcgid/installer.pm
+++ engine/PerlLib/Servers/httpd/apache_fcgid/installer.pm
@@ -336,7 +336,7 @@ sub _buildFastCgiConfFiles
     $rs ||= $file->owner( $main::imscpConfig{'ROOT_USER'}, $main::imscpConfig{'ROOT_GROUP'} );
     $rs ||= $file->mode( 0644 );
     $rs = $self->{'httpd'}->disableModules(
-        'actions', 'fastcgi', 'fcgid', 'fcgid_imscp', 'php5', 'php5_cgi', 'php5filter', 'php5.6', 'php7.0', 'php7.1',
+        'actions', 'fastcgi', 'fcgid', 'fcgid_imscp', 'php5', 'php5_cgi', 'php5filter', 'php5.6', 'php7.0', 'php7.1', 'php7.2', 'php7.3',
         'proxy_fcgi', 'proxy_handler', 'mpm_itk', 'mpm_event', 'mpm_prefork', 'mpm_worker'
     );
     $rs ||= $self->{'httpd'}->enableModules( 'actions', 'authz_groupfile', 'fcgid_imscp', 'mpm_event', 'version' );
@@ -419,8 +419,8 @@ sub _buildApacheConfFiles
     $rs ||= $self->{'httpd'}->enableSites( '00_nameserver.conf' );
     $rs ||= $self->{'httpd'}->enableConfs( '00_imscp.conf' );
     $rs ||= $self->{'httpd'}->disableConfs(
-        'php5.6-cgi.conf', 'php7.0-cgi.conf', 'php7.1-cgi.conf',
-        'php5.6-fpm.conf', 'php7.0-fpm.conf', 'php7.1-fpm.conf',
+        'php5.6-cgi.conf', 'php7.0-cgi.conf', 'php7.1-cgi.conf', 'php7.2-cgi.conf', 'php7.3-cgi.conf',
+        'php5.6-fpm.conf', 'php7.0-fpm.conf', 'php7.1-fpm.conf', 'php7.2-fpm.conf', 'php7.3-fpm.conf',
         'serve-cgi-bin.conf'
     );
     $rs ||= $self->{'httpd'}->disableSites( 'default', 'default-ssl', '000-default.conf', 'default-ssl.conf' );
--- engine/PerlLib/Servers/httpd/apache_itk/installer.pm
+++ engine/PerlLib/Servers/httpd/apache_itk/installer.pm
@@ -314,8 +314,8 @@ sub _buildPhpConfFiles
         }
     );
     $rs = $self->{'httpd'}->disableModules(
-        'actions', 'fastcgi', 'fcgid', 'fcgid_imscp', 'suexec', 'php5', 'php5_cgi', 'php5filter', 'php5.6', 'php7.0',
-        'php7.1', 'proxy_fcgi', 'proxy_handler', 'mpm_itk', 'mpm_event', 'mpm_prefork', 'mpm_worker'
+        'actions', 'fastcgi', 'fcgid', 'fcgid_imscp', 'suexec', 'php5', 'php5_cgi', 'php5filter', 'php5.6', 'php7.0', 'php7.1', 'php7.2', 'php7.3',
+        'proxy_fcgi', 'proxy_handler', 'mpm_itk', 'mpm_event', 'mpm_prefork', 'mpm_worker'
     );
     $rs ||= $self->{'httpd'}->enableModules(
         'authz_groupfile', "php$self->{'phpConfig'}->{'PHP_VERSION'}", 'mpm_itk', 'version'
@@ -397,8 +397,8 @@ sub _buildApacheConfFiles
     $rs ||= $self->{'httpd'}->enableSites( '00_nameserver.conf' );
     $rs ||= $self->{'httpd'}->enableConfs( '00_imscp.conf' );
     $rs ||= $self->{'httpd'}->disableConfs(
-        'php5.6-cgi.conf', 'php7.0-cgi.conf', 'php7.1-cgi.conf',
-        'php5.6-fpm.conf', 'php7.0-fpm.conf', 'php7.1-fpm.conf',
+        'php5.6-cgi.conf', 'php7.0-cgi.conf', 'php7.1-cgi.conf', 'php7.2-cgi.conf', 'php7.3-cgi.conf',
+        'php5.6-fpm.conf', 'php7.0-fpm.conf', 'php7.1-fpm.conf', 'php7.2-fpm.conf', 'php7.3-fpm.conf',
         'serve-cgi-bin.conf'
     );
     $rs ||= $self->{'httpd'}->disableSites( 'default', 'default-ssl', '000-default.conf', 'default-ssl.conf' );
